27cc1686fda77f5094d077798ec42ba8fc56a02c
Navjot Singh <navjotd139@gmail.com>
2019-06-20 17:53:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.notification;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager notifyManager;
+    private static final int NOTIFICATION_ID = 0;
+
+    private Button button_notify;
+    private Button button_cancel;
+    private Button button_update;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_notify = findViewById(R.id.notify);
+        button_cancel = findViewById(R.id.cancel);
+        button_update = findViewById(R.id.update);
+
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification();
+            }
+        });
+
+        button_cancel.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                cancelNotification();
+            }
+        });
+
+        button_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateNotification();
+            }
+        });
+        createNotificationChannel();
+    }
+
+    public void sendNotification() {
+
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notifyManager.notify(NOTIFICATION_ID,notifyBuilder.build());
+    }
+
+    public void updateNotification() {
+        Bitmap image = BitmapFactory.decodeResource(getResources(),R.drawable.mascot_1);
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notifyBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(image)
+                .setBigContentTitle("Notification Updated!").bigLargeIcon(image));
+        notifyManager.notify(NOTIFICATION_ID,notifyBuilder.build());
+    }
+
+    public void cancelNotification() {
+        notifyManager.cancel(NOTIFICATION_ID);
+    }
+
+    public void createNotificationChannel() {
+        notifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification", NotificationManager
+                    .IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            notifyManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder() {
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this,PRIMARY_CHANNEL_ID) .setContentTitle("You've been notified!")
+                .setContentText("This is your notification text.")
+                .setSmallIcon(R.drawable.ic_android);
+        return notifyBuilder;
+    }
+
+}
